b4020ef9e7adef1cbb872292cf6f3e4c9cd423b6
kaverna <guilherme.ssilva@gmail.com>
2016-02-08 18:00:14 -0200

    Lista de referências para consultas

---
+++ referencias.php
@@ -0,0 +1,29 @@
+<?php
+
+require('header.php');
+
+?>
+
+<div class="container marketing" style="padding-top: 70px;">
+
+    <h2 class="featurette-heading" style="padding-top: 60px; padding-bottom: 40px;" id="referencias">
+        
+        Muito Obrigado. 
+    </h2>
+        
+
+    <p> <a href="http://fi.co" > <b> The Founder Institute: </b></a> </p>
+ 
+    <p> <a href="http://fi.co" > <b> The Founder Institute: </b></a> </p>
+     
+    <p> <a href="http://agenciarj.org/" > <b> Agência de Redes para Juventude: </b></a> </p>
+ 
+</div>
+
+
+<?php
+
+require('footer.php');
+
+?>
+
