a9987b737c48a69b5438b4090f7f621b5b3f4f3b
kaverna <guilherme.ssilva@gmail.com>
2016-02-25 18:52:57 -0300

    Criação de Login e página de Projetos

--- login.php
+++ login.php
@@ -1,46 +1,21 @@
-<?php
-           $config = dirname(__FILE__) . '/auth/config.php';
-           require_once( dirname(__FILE__) . "/auth/Hybrid/Auth.php" );
-           require_once("db.php");
-           
- 
-           
-           try{
-               $hybridauth = new Hybrid_Auth( $config );
- 
-               $facebook = $hybridauth->authenticate( "Facebook" );
- 
-               $user_profile = $facebook->getUserProfile();
-                
-              // $facebook->setUserStatus( "Hello world!" );
-               
-               $email=  db_quote($user_profile->email);
-               
-               $user_bd = db_select_single_row("SELECT * FROM `Logins` WHERE `email`=$email;");
- 
-               if (empty($user_bd)) {
-                   
-                   $fbID=  db_quote($user_profile->identifier);
-                   $sql= "INSERT INTO `Logins`(`email`, `facebookID`, `admin`) VALUES ( $email , $fbID ,false);";
-                   db_query($sql);
-               }
-               
-               
-               $user_contacts = $facebook->getUserContacts();
-           }
-           catch( Exception $e ){
-               echo "Ooophs, we got an error: " . $e->getMessage();
-           }
-           
-           ?><pre>
-           
-<?php print_r($user_profile); ?>
-           
-           </pre>
+<?php require_once 'header.php';
 
-<pre>
-           
-<?php print_r($user_contacts); ?>
-           
-           </pre>
+?>
 
+<div class="container marketing" style="padding-top: 90px;">
+
+  
+<form>
+    <fieldset>
+        <legend>Use o Facebook para logar:</legend>
+
+        <a href="callback.php?provider=facebook"><img src="img/basic_social/colored/64/rounded_square/facebook.png" alt="Facebook"/></a>
+        <!-- a href="callback.php?provider=twitter" ><img src="img/basic_social/colored/64/rounded_square/twitter.png"  alt="Twitter" /></a><br />
+        <a href="callback.php?provider=linkedin"><img src="img/basic_social/colored/64/rounded_square/linkedin.png" alt="LinkedIn" /></a> -->
+    </fieldset>
+</form>
+
+</div>      
+<?php require_once 'footer.php';
+
+?>
